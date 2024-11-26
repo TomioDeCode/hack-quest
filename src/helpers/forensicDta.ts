2244bb5cc9b06116988fafe876a86e334f8dba11
TomioDeCode <tomiodecode@gmail.com>
2024-11-26 13:11:02 +0700

    fix: eslint

--- src/helpers/forensicDta.ts
+++ src/helpers/forensicDta.ts
@@ -1,18 +1,39 @@
 export const challenges = [
   {
     id: "1",
-    title: "File Signature Analysis",
+    title: "Scan Surprise",
     description:
-      "Sebuah file memiliki signature hex berikut: 89 50 4E 47 0D 0A 1A 0A. Tentukan tipe file tersebut.",
-    flag: "PNG",
-    hint: "File signatures sering disebut sebagai 'magic numbers' dan digunakan untuk mengidentifikasi tipe file.",
+      "Saya sudah bosan membagikan bendera dalam bentuk teks. Bukankah lebih keren jika bendera disajikan dalam bentuk gambar?",
+    flag: "CTF{shp2H4tk6OQTVSOH}",
+    hint: "Penggunaan tools zbarimg pada arsitektur 64-bit terbukti sangat membantu dalam mempermudah pekerjaan.",
   },
   {
     id: "2",
-    title: "Scan Surprise",
+    title: "Secret of the Polyglot",
     description:
-      "Sebuah gambar JPG bernama 'hidden_message.jpg' diduga menyembunyikan pesan rahasia. Gunakan tool steganography untuk mengekstrak pesan tersebut. Flag adalah pesan yang ditemukan.",
-    flag: "CTF{ST3G0_M4ST3R}",
-    hint: "Tools seperti steghide atau online steganography decoder bisa membantu mengekstrak pesan tersembunyi dari gambar.",
+      "Sebuah gambar JPG bernama 'flag2.jpg' diduga menyembunyikan pesan rahasia. Gunakan tool steganography untuk mengekstrak pesNOC lokal menemukan file mencurigakan dengan tipe yang tidak jelas. Sebagai ahli, tugas Anda adalah memeriksa file tersebut dan mengungkap semua informasi tersembunyi.an tersebut. Flag adalah pesan yang ditemukan.",
+    flag: "CTF{The_Secret_Of_Polyglot_Unlocks_Potential}",
+    hint: "Masalah ini dapat diselesaikan hanya dengan membuka file dengan cara yang berbeda.",
+  },
+  {
+    id: "3",
+    title: "CanYouSee",
+    description: "Bagaimana kalau bermain petak umpet?",
+    flag: "CTF{this_is_a_hidden_flag}",
+    hint: "Penggunaan tools exiftool sangat berguna untuk saat ini..",
+  },
+  {
+    id: "4",
+    title: "Glory of the Garden",
+    description: "Taman ini berisi lebih dari apa yang terlihat.",
+    flag: "CTF{Glory_Of_The_Garden_Blooms_Eternal}",
+    hint: "Apa itu editor hex?",
+  },
+  {
+    id: "5",
+    title: "Hide Me",
+    description: "SOC menemukan gambar mencurigakan yang dikirim antar pengguna. Tugas Anda adalah menyelidiki dan mengungkap apa yang tersembunyi di dalamnya.",
+    flag: "CTF{Hidden_In_Plain_Sight}",
+    hint: "Tidak Ada Petunjuk",
   },
 ];
