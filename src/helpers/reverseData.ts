cb8a49c5109cd94bd29cda0dd4422a04867b1c8f
TomioDeCode <tomiodecode@gmail.com>
2024-12-03 16:25:21 +0700

    feat: flag

--- src/helpers/reverseData.ts
+++ src/helpers/reverseData.ts
@@ -41,7 +41,7 @@ export const reverseData = [
     id: "reverse-5",
     title: "Reverse String",
     description: "Balikkan string berikut: }GHVUHYHU_JQLUWV{FTC",
-    flag: "CTF{STRING_REVERSED}",
+    flag: "CTF{VWTLQJ_UHUYHUVH}",
     file: "",
     url: "",
     hint: "Tidak Ada Petunjuk",
